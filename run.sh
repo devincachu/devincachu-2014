55882557cd16a91c4a15fdcb626e7007b8204d23
Francisco Souza <f@souza.cc>
2014-03-15 11:06:03 -0300

    run: remove file

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-my_dir=`dirname $0`
-cd $my_dir
-export `circusctl options gunicorn-devincachu | grep ^env: | cut -f2 -d' ' | sed -e 's/,/\nexport /g'`
-export PATH="$PWD/env/bin:$PATH"
-exec $*
