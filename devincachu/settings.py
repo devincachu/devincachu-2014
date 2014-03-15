b7d4be3e129408241641241317a63d5835d6b7e1
Francisco Souza <f@souza.cc>
2014-03-15 11:10:36 -0300

    settings: remove htmlmin middleware

--- devincachu/settings.py
+++ devincachu/settings.py
@@ -85,7 +85,6 @@ MIDDLEWARE_CLASSES = (
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "htmlmin.middleware.HtmlMinifyMiddleware",
     "django.contrib.flatpages.middleware.FlatpageFallbackMiddleware",
 )
 
